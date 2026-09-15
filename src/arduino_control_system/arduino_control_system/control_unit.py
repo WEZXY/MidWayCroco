d2f4b67e3f377b92b3277d77d2b7cdc445daf8dc
el-moataz <almoatazmohamednabil@gmail.com>
2026-09-15 19:44:23 +0300

    first commit, added ir and buzzer logic

--- src/arduino_control_system/arduino_control_system/control_unit.py
+++ src/arduino_control_system/arduino_control_system/control_unit.py
@@ -8,15 +8,28 @@ class Robot(Node):
         super().__init__('Control_Unit')
         self.counter=0.0
         self.get_logger().info('Starting Control Unit')
-        self.arduino_reading_sub = self.create_subscription(ArduinoReading, 'arduino_reading', self.reading_callback, 10)
-        self.cmd_vel_pub = self.create_publisher(ArduinoActions, 'arduino_writing', 10)
+        self.arduino_reading_sub = self.create_subscription(
+            ArduinoReading, 'arduino_reading', self.reading_callback, 10)
+        self.cmd_vel_pub = self.create_publisher(
+            ArduinoActions, 'arduino_writing', 10)
         self.create_timer(0.1, self.timer_callback)
 
+        self.ir_value = None
+        self.ir_alert = False 
+
     def reading_callback(self, msg):
-        pass
+        self.ir_value = msg.ir
 
     def timer_callback(self):
-        pass
+        self.ir_sensor()
+
+        action_msg = ArduinoActions()
+        action_msg.buzzer = self.buzzer()
+
+
+
+
+        self.cmd_vel_pub.publish(action_msg)
 
     # readings
     def temperature_sensor(self):
@@ -26,7 +39,10 @@ class Robot(Node):
         pass
 
     def ir_sensor(self):
-        pass
+        if self.ir_value is None:
+            return 
+        ir_threshold = 150 #n4of el 7war da b3den 
+        self.ir_alert = self.ir_value > ir_threshold
 
     def light_sensor(self):
         pass
@@ -45,7 +61,7 @@ class Robot(Node):
         pass
 
     def buzzer(self):
-        pass
+        return self.ir_alert 
 
     def light(self):
         pass
